30dbd478ca21ad5e5bca8530764b05c61b0144a8
BalaVasanth <balavasanth.polimetla@blueyonder.com>
2021-12-14 14:07:09 +0530

    code updates

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
-import { Box, Paper, Typography } from '@material-ui/core';
-
+import { Box, IconButton } from '@material-ui/core';
+import { LuiBackground, LuiCardComponent } from '@jda/lui-common-component-library';
+import { X } from '@jda/lui-common-icon-library';
 const Home = () => {
+  const cardJson = [
+    {
+      title: 'MFE_1',
+      subTitle: 'Warehouse Management',
+      description: 'Demo content goes Here',
+    },
+    {
+      title: 'MFE_2',
+      subTitle: 'Order Management',
+      description: 'Demo content goes Here',
+    },
+  ];
   return (
     <Box overflow="hidden">
       <Box display="flex" justifyContent="flex-start">
-        <Paper>
+        <LuiBackground>
           <Box p={2}>
-            <Typography variant="h4">
-              ALM React App.., click on the right panel navigation to switch MFE
-            </Typography>
+            {cardJson.map((item, index) => (
+              <Box m={1} key={index}>
+                <LuiCardComponent
+                  title={item.title}
+                  headerBordered
+                  titleVariant="h5"
+                  titleActions={
+                    <Box>
+                      <IconButton size="small" color="secondary" aria-label="Close Icon Button">
+                        <X color="action" />
+                      </IconButton>
+                    </Box>
+                  }
+                >
+                  <Box>{item.subTitle}</Box>
+                  <Box>{item.description}</Box>
+                </LuiCardComponent>
+              </Box>
+            ))}
           </Box>
-        </Paper>
+        </LuiBackground>
       </Box>
     </Box>
   );
 };
-
 export default Home;
